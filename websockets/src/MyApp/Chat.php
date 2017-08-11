d23479cf8407d1a34e6bb7ea31abbd56d22fef2c
edalmava <eamartinezv@ufpso.edu.co>
2017-08-11 17:09:07 -0500

    110820171708001

--- websockets/src/MyApp/Chat.php
+++ websockets/src/MyApp/Chat.php
@@ -3,37 +3,66 @@ namespace MyApp;
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+date_default_timezone_set('America/Bogota');
+
 class Chat implements MessageComponentInterface {
-    protected $clients;
+    protected $clients;	
 
     public function __construct() {
-        $this->clients = new \SplObjectStorage;
+        $this->clients = new \SplObjectStorage;		
     }
 
-    public function onOpen(ConnectionInterface $conn) {
-        // Store the new connection to send messages to later
+    public function onOpen(ConnectionInterface $conn) {        
         $this->clients->attach($conn);
-
-        echo "New connection! ({$conn->resourceId})\n";	
-
-        foreach ($this->clients as $client) {
-            if ($conn !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send("Usuario{$conn->resourceId}: CONECTADO");
-            }
-        }			
+		
+        echo "New connection! ({$conn->resourceId})\n";	        
+		
+		$msg = new \stdClass;
+		$msg->type = "id";
+		$msg->id = $conn->resourceId;		
+		$conn->send(json_encode($msg));	
     }
+	
+    private function hacerMensajeListaUsuarios() {
+		$userListMsg = new \stdClass;
+		$userListMsg->type = "userlist";
+		$userListMsg->users = array();		
+		
+		foreach ($this->clients as $client) {
+			$userListMsg->users[] = $client->username;
+		}
+		
+		return $userListMsg;
+	}
+	
+	private function enviarListaUsuariosTodos() {
+		$userListMsg = $this->hacerMensajeListaUsuarios();
+		foreach ($this->clients as $client) {
+			$client->send(json_encode($userListMsg));
+		}
+	}
 
     public function onMessage(ConnectionInterface $from, $msg) {
         $numRecv = count($this->clients) - 1;
         echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
             , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+			
+		$msg_json = json_decode($msg);
+		
+		switch($msg_json->type) {
+			case "message":
+			    $msg_json->name = $from->username;
+				//TODO: falta sanitizar el texto del mensaje
+				break;			
+			case "username": 
+			    //TODO: falta sanitizar el nombre del usuario
+				$from->username = $msg_json->name;
+				$this->enviarListaUsuariosTodos();
+			    break;
+		}
 
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send("Usuario{$from->resourceId}: $msg");
-            }
+        foreach ($this->clients as $client) {            
+			$client->send(json_encode($msg_json));            
         }
     }
 
@@ -42,12 +71,17 @@ class Chat implements MessageComponentInterface {
         $this->clients->detach($conn);
 
         echo "Connection {$conn->resourceId} has disconnected\n";
-
+		
+		$this->enviarListaUsuariosTodos();
+		
+		$msg = new \stdClass;
+		$msg->type = "desconexion";
+		$msg->name = $conn->username;
+        $msg->date = date('Y-m-d H:i:s');		
+						
         foreach ($this->clients as $client) {
-            if ($conn !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send("Usuario{$conn->resourceId}: DESCONECTADO");
-            }
+            //if ($conn !== $client) {}
+			$client->send(json_encode($msg));
         }		
     }
 
